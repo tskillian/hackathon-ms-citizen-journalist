c4761f8cf87785a31356ff38b7e531d82ba0d2e6
Matthew Bentley <matthew.t.bentley@gmail.com>
2013-12-07 10:12:28 -0800

    Changed to hashtags instead of people.

--- mainpage.py
+++ mainpage.py
@@ -13,12 +13,12 @@ JINJA_ENVIRONMENT = jinja2.Environment(
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
 
-class Person(db.Model):
+class Hashtag(db.Model):
     name=db.StringProperty()
     Location=db.StringProperty()
     
 class Questions(db.Model):
-    contact = db.ReferenceProperty(Person, collection_name="questions")
+    contact = db.ReferenceProperty(Hashtag, collection_name="questions")
     question = db.StringProperty()
 
 class MainPage(webapp2.RequestHandler):
@@ -32,30 +32,40 @@ class AskQuestion(webapp2.RequestHandler):
     
     def get(self):
         self.response.headers['Content-Type'] = 'text/html'
-        self.response.write("<html><body><form action='/add-q' method='post'>\n<br />Question for: <input type=text name='person' />\n" + \
+        self.response.write("<html><body><form action='/add-q' method='post'>\n<br />Question for: <input type=text name='hashtag' />\n" + \
                             "<br />Your question (500 char max): <textarea rows=6 cols=40 maxlength=500 name='question'></textarea>1\n" + \
                             "<br /><input type='submit' /></form></body></html>")
         
 class AddQ(webapp2.RequestHandler):
     
     def post(self):
-        person = Person(name=self.request.get('person').lower(), key_name=self.request.get('person').lower())
-        person.put()
-        Questions(contact=person, question=self.request.get('question')).put()
+        hashtag = Hashtag(name=self.request.get('hashtag').lower().replace("#",""), key_name=self.request.get('hashtag').lower().replace("#",""))
+        hashtag.put()
+        Questions(contact=hashtag, question=self.request.get('question')).put()
         self.response.write('Success. Maybe.')
         
-class GetPerson(webapp2.RequestHandler):
+class GetHashtag(webapp2.RequestHandler):
     """Sends json encoded information about a person"""
     def get(self):
-        info = db.GqlQuery("SELECT * FROM Person WHERE name=:1", self.request.get('person'))
-        info = info.run().next()
+        info = db.GqlQuery("SELECT * FROM Hashtag WHERE name=:1", self.request.get('hashtag'))
+        results = True
+        try:
+            info = info.run().next()
+        except:
+            questions=[]
+            results = False
         to_send = {}
-        to_send['name'] = info.name.title()
-        questions = []
-        for q in info.questions:
-            questions.append(q.question)
+        if results:
+            to_send['name'] = info.name
+            questions = []
+            for q in info.questions:
+                questions.append(q.question)
+                print q.question
+        to_send['result'] = results
         to_send['questions'] = questions
-        self.response.write(json.dumps(to_send))
+        json_to_send = str(json.dumps(to_send))
+        string_to_send = self.request.get('callback') + "(" + json_to_send + ")"
+        self.response.write(string_to_send)
         
 class Callback(webapp2.RequestHandler):
     
@@ -79,7 +89,7 @@ class Login(webapp2.RequestHandler):
 app = webapp2.WSGIApplication([
                                ('/login', Login),
                                ('/callback', Callback),
-                               ('/get-person', GetPerson),
+                               ('/get-hashtag', GetHashtag),
                                ('/ask-question', AskQuestion),
                                ('/add-q', AddQ),
                                ('/index', MainPage),
